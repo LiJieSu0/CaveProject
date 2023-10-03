5890fe5745bb0c827e0a320fca728d931cf494c0
LiJieSu0 <opwinopwin@gmail.com>
2023-10-03 00:13:44 -0400

    enemy detection init

--- Scenes/Characters/beetle.cs
+++ Scenes/Characters/beetle.cs
@@ -6,6 +6,7 @@ public partial class beetle : CharacterBody2D
 	public const float Speed = 300.0f;
 	public int hitCount=0;
 	RayCast2D sight;
+	private bool isDetectingPlayer=false;
 	public override void _Ready()
 	{
 		bullet.bulletHit+=hitByBullet;
@@ -15,6 +16,8 @@ public partial class beetle : CharacterBody2D
 	public override void _Process(double delta)
 	{
 		SightDetection();
+		if(isDetectingPlayer)
+			LookAt(GetParent().GetNode<Node2D>("Player").Position);
 		//TODO patrol AI, follow AI, Attack AI	
 	}
 	public void hitByBullet(Node2D body){
@@ -28,7 +31,12 @@ public partial class beetle : CharacterBody2D
 
 	public void OnBodyEnterDetectArea(Node2D body){
 		if(body.Name=="Player"){
-			LookAt(body.Position);
+			isDetectingPlayer=true;
+		}
+	}
+	public void OnBodyExitDetectArea(Node2D body){
+		if(body.Name=="Player"){
+			isDetectingPlayer=false;
 		}
 	}
 	public void SightDetection(){
